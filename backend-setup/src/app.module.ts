f3fc57ba0523d6acd64e866c727aad7a8069b995
Alessia <98785381+amorea9@users.noreply.github.com>
2023-01-27 15:39:13 +0100

    implementing typeorm

--- backend-setup/src/app.module.ts
+++ backend-setup/src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsFactsModule } from './catfacts/catfacts.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import entities from './typeorm';
 
 @Module({
-  imports: [CatsFactsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: entities,
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+
+    CatsFactsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
